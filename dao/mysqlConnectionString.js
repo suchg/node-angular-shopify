be8ea3739e57e8c14c6258b17000b46cc64e0749
sachin.gavas <sachin.gavas@pubmatic.com>
2020-08-01 20:33:34 +0530

    db changed to heroku

--- dao/mysqlConnectionString.js
+++ dao/mysqlConnectionString.js
@@ -1,18 +1,24 @@
 var mysqlConnectionString = {
-  connection: {
-      dev: {
-          host: 'localhost',
-          user: 'subuser',
-          password: 'Subuser@123',
-          database: 'subscription'
-      },
-      qa: {
-          host: 'localhost',
-          user: 'subuser',
-          password: 'Subuser@123',
-          database: 'subscription'
-      }
-  }
+    connection: {
+        prod: {
+            host: 'us-cdbr-east-02.cleardb.com',
+            user: 'b7ff08adeb1efd',
+            password: 'bf55b92f',
+            database: 'heroku_8fa48d4d9e6263e'
+        },
+        dev: {
+            host: 'us-cdbr-east-02.cleardb.com',
+            user: 'b7ff08adeb1efd',
+            password: 'bf55b92f',
+            database: 'heroku_8fa48d4d9e6263e'
+        },
+        qa: {
+            host: 'localhost',
+            user: 'subuser',
+            password: 'Subuser@123',
+            database: 'subscription'
+        }
+    }
 }
 
 module.exports.mysqlConnectionString = mysqlConnectionString;
