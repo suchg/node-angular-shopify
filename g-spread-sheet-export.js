cc6c2371de8e211f23c1fb3d843ab312fed4ed0d
sachin <sachin.gavas99@gmail.com>
2020-08-27 21:17:44 +0530

    changed google account details

--- g-spread-sheet-export.js
+++ g-spread-sheet-export.js
@@ -1,9 +1,9 @@
 const gsr = require('./g-spread-sheet/lib/index')
 const dotenv = require('dotenv').config();
 var cron = require('node-cron');
-const email = 'sachin@unlikely-flowrist2.iam.gserviceaccount.com';
-const key = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDCCZ07V1qAhvgU\nQKVYjIMP2Unxy3szBzGQnz7cS6xSvS8QnpZVe9m9q4jbOk7KjguFED67y5ZGZYqz\nJDQjxn0v2znBYXRsw+GN7o/OJrwhoRnbzjn76EVxnwfJgWeaeaxetQcPkDrFuie7\nH2hPRrtcGVwfiG0zXfIKxZ3oKeHTip4dgc+jXgy0rBan/ujK/23yYeBW+FOXVtxx\nPVZNJZakgMHgyE0+tgnq514YmhfruKEFL16sj9FtBP849kOg3Bw2ZndpGxmw+1nk\nGI2VkJfvCJ4y5A0B9kmKURZzJqWBvGe8Ctq8q/KzwBTJiHyTeoIL5jH+2/0cV14Y\ntSoP0cKtAgMBAAECggEACsuqpA/Q4PhBaq0LFgFhVFsoRkvmK5lhnFSD1JFgQc01\nRmUwY4qru2YFC/9zPUlIYkucVen2jy2cmSlxPjFPqz80+CmiJuACF1m3xgY5LtzN\n+cEAUF/kpzTsI72vpDRTTwSe4zKI1pHsHnjZnpmGmJHvonB+dN5r9kX26mEUJ6cE\njFS0yN7MRwJlkxAUwAXFYOYA5stI4q7GQkVaWr4JLJE/6s6vUnDxrWq5qmKiaGCC\nqKG5F97CwaYFs+2p4HYduGbYkiHmHvKrcie2xA8rANKeauSCgoeC1/0I62MjAS5H\nDxO2C5o18M9BbumIFuK54BzUfqvKYUlYNDC4AALoUQKBgQD0G0rx/6Uuc0xS7isc\n1UnqF75HOe5r/czMuSmy3IBBln6JaUpQbhMMqHFVlyk5xTk0KLQML8Hss387N56U\nnHDXAtFZiNVH37MraJ4okOeKqZI2NWlJ7oMj1XDxQg/Xx3js48Rnupe1yHogxsf9\n7Z9+8tnZdzN2fuLA9OlyESGfPQKBgQDLfdEx4pau+6lodPV1I5JY5EiWBVj7jxod\n9+OSTWQYpmukAW2npHlY0nMCKVetXy4IZksApvmf9hNsSExXTLwjcnw2QfiJY9c0\net6/HMgmbKXwP2k3ceEshFEiweizjWpOZSX8ixd7a9j0OiY+w20vzBom5FlzAPYQ\nBF11oY/oMQKBgQCxNfMVdcqbyxYYilCAERMkv3HiUVsVyN1Zrf5om4z8kF1yMh7B\n/vcTVGFaim9iQhxCEl6LDkyRIsn12m3kYyHD5YbHof97uxleJQfUWUXOM0Yy1lmG\nU4GejP/+FUBdHLFcAAoAZ8RlMpH+o3BilpQYXALszY1ts7W38NRce7VV3QKBgQCI\nWJ0616AvTuCL5RIykssUVKFuI9cjdxcAzX/mH4TaH2gwU+StVz8XjKsS37Cnsgae\nCixnG3pW/AK3oxnDxN6qwaMIl9t38zXXLBzj4N0bIeFtwI83X06JDX9v0o2hATgh\nnxR8ypXJVNHhkrTdiQQ36oH7XTWyIQq+YK5eiwppgQKBgBiX9qr26lzm3mR1wwEh\nlfc37I0ByfOG/SC9LoPNjYEKgVmR/eu9x+ptBPeV8v6dTCZj9XzYRPdDp/gdhG7o\nkkDVvRETofZ6VTGPRM/LLzXBpLP4t2W134OW5P7Kt1mZ0mPASLajajDiVC17d5r2\nLCIqM4shKE/b8y2gV137ACg3\n-----END PRIVATE KEY-----\n";
-const spreadsheetId = '1h51GbILg0mnqy9ZqhZr-26ZzKU_qqHX6T7mer5V9c_M';
+const email = 'context@tufsubscribe.iam.gserviceaccount.com';
+const key = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCbnWbLzf7t4TJZ\nhuGRjahj8aNlpLOUofBPSX17o4ONqbZJloOjF1qeTRUpi0KuBieUaOKu+1tGLv4f\nigK1eQGdCbAey5xtCNLR95XzPkxNxTurYuI8VkKskQb5fNGIi80sjwRJLmw7ZQ7O\na3f/eEq1C/s4A3089dS3QGHTFpYu3NOccej5tZ/7H4fhfarkHHP6nmkOLA3GXcJP\n1k3abzCYQz3YKGePMWR+3rz+tWot7VQAsNbew5RNOV4Cn2Cht8UH6TXYqnGRVgoV\ngZU672bWupI5aYZpuBspJT0ya+QYRBBEpFBT9uEog6nhmWxGxpUsfOq3rglW+nl0\nPFRR/bRZAgMBAAECggEAJnWuKogorZeW90/x72SMndjiepRMattkFmByIizNLmWn\nnwPTWGtE8vNXm9smeXadcR1ECx2l1xB1r4tPNJUpzCtRCydk64C/A/Q2K/Y1axIo\nN2k6w1FJbLdyOxZ5fc8ZgB3/n+uYhcxp7qPadjn/csP5jIQ2P2r9beX8V2yRZRa9\njpPlcH2cfoa+jTGHuJdamgOh27vu0zvMkdhIqf4Y3sN/+8W9sQtpCZMKpvwIBAM2\n04jPPe6uDHPJcStr7lhbv+DDyUnKwUDu2h5oamazfWKFTSCWxQXtEIQFM7KKYal5\nKdTX7Z0UNAlcY2eW6eaUVJ6w3araQWRXMqkCK/t3hQKBgQDJ7j/QKdmOif16b2YU\nA1KfjZdJCRXqsw6yCwhKJr1rFTf2DyiJdj8R+p7alLgjMEw7kYiA6vXpirl6rdlw\nZuhY5zulmygvOP4m/DNw8RQr0LGuWjq/L+EwucIISuVt8+29tH5LwLibu3JJrM1A\nbboR2z7VsCepmfBcoNvWMQhATQKBgQDFSFcSo0vUnxxCOluhxBK9Z2Cz6ixw68tD\nd5/kFFCps3MLlDr9wzO3AIYyrhlItnqPov4LcWuGdg1551AU7NGwXNcOqTAmlSaR\nxMd1JY/bhgZRn9hml2ouzh9M4qE03g7UpoVFe9Dh92K54mnduR9hcp1gJkpFrAAB\n+wpnmUDqPQKBgQCim9gJYloOfbKAcMqr3Q0g7R1htA5RJIX1/Apd5Bpu6/RXkldb\ncrxRVxeq/03VEGSjJ4vb/NmwioTYdLz/1e7PAM44itxuQp/vdvJZxdkve2xJ8eES\n6pSV71B/6wgcZe0R8hIuIHDI/8aF68CRXIsVLA/KdoNKAWvcu7CsKGnzlQKBgC/A\nZcsOGZG6fYWOSJWsNvj/Z8nEmcMvX8DR2LLFNA0PcX5Q/8JimEboapMjztzpxlq0\nEhfE+UjPlE14bsrR06ODpU5YJ15/ZZmM7tZtJBf3pdnl/eQ/LCVuIOdkR67bPO29\n7K433QSE87GYmxVe9LiKnIskWX/ptQrFr+wJjUvlAoGADcqvgLAHROcMbMWJ3CxW\nGBTTjdOqVxPBRd3vKNLssU2lvNLuC8RRfTnYCFXxIw22BcfVk0wBMXgZ8w0droHY\nEMKI5O0rxOm2Wb+EJb4In2m5Z+f1fPp4Ji79nvCo01BwiWRUqkCRQmioRkns3vMl\nktj0pZCJNIoG5vENsRVkwL4=\n-----END PRIVATE KEY-----\n";
+const spreadsheetId = '1v1RmfuFLxa6uKGRs55hJsymV_4kqK1QiP8oMmpfXY7s';
 // const spreadSheetName = 'orders';
 // const gReadData = require('./g-read-data');
 var moment = require('moment');
@@ -184,7 +184,7 @@ const initSubscriptionOrdersExport = () => {
 };
 
 const startExportCron = () => {
-    cron.schedule('0 0 */3 * * *', () => {
+    cron.schedule('0 */6 * * * *', () => {
         initOrdersExport();
         initUpcomingOrdersExport();
         initSubscriptionOrdersExport();
